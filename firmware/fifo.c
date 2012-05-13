8e852fd35da546e28ef77a67ad97a8cf89789abf
Fabio Baltieri <fabio.baltieri@gmail.com>
2012-05-13 12:59:11 +0200

    fw: add fifo files

---
+++ firmware/fifo.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#include "fifo.h"
+
+struct fifo rf_tx_fifo;
+struct fifo rf_rx_fifo;
+
+uint8_t *fifo_get_head(struct fifo *fifo)
+{
+	if (fifo->count < FIFO_SIZE)
+		return fifo->entry[fifo->head].data;
+	else
+		return NULL;
+}
+
+void fifo_push(struct fifo *fifo)
+{
+	fifo->head = (fifo->head + 1) % FIFO_SIZE;
+	fifo->count++;
+}
+
+uint8_t *fifo_get_tail(struct fifo *fifo)
+{
+	if (fifo->count)
+		return fifo->entry[fifo->tail].data;
+	else
+		return NULL;
+}
+
+void fifo_pop(struct fifo *fifo)
+{
+	fifo->tail = (fifo->tail + 1) % FIFO_SIZE;
+	fifo->count--;
+}
+
+uint8_t fifo_count(struct fifo *fifo)
+{
+	return fifo->count;
+}
+
+void fifo_clear(struct fifo *fifo)
+{
+	fifo->head = 0;
+	fifo->tail = 0;
+	fifo->count = 0;
+}
---
+++ firmware/fifo.h
@@ -0,0 +1,23 @@
+#define PAYLOAD_SIZE	16
+#define FIFO_SIZE	8
+
+struct entry {
+	uint8_t data[PAYLOAD_SIZE];
+};
+
+struct fifo {
+	uint8_t head;
+	uint8_t tail;
+	uint8_t count;
+	struct entry entry[FIFO_SIZE];
+};
+
+extern struct fifo rf_tx_fifo;
+extern struct fifo rf_rx_fifo;
+
+uint8_t *fifo_get_head(struct fifo *fifo);
+void fifo_push(struct fifo *fifo);
+uint8_t *fifo_get_tail(struct fifo *fifo);
+void fifo_pop(struct fifo *fifo);
+uint8_t fifo_count(struct fifo *fifo);
+void fifo_clear(struct fifo *fifo);
