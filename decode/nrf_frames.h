c4f90453d9076f5046f5963b9238f7342b95ebad
Fabio Baltieri <fabio.baltieri@gmail.com>
2013-12-25 20:24:41 +0000

    nrf_frames: add boot defines

--- decode/nrf_frames.h
+++ decode/nrf_frames.h
@@ -2,6 +2,7 @@
 
 #define NRF_MSG_ID_GENERIC	0x00
 #define NRF_MSG_ID_POWER	0x01
+#define NRF_MSG_ID_BOOT		0xff
 
 #define NRF_POWER_VBATT_CHARGING	(1 << 15)
 #define NRF_POWER_VBATT_MASK		0x7fff
@@ -11,9 +12,23 @@ struct nrf_power {
 	uint8_t _spare[2];
 };
 
+#define NRF_BOOT_CMD_ACK	0x00
+#define NRF_BOOT_CMD_BOOT	0x01
+#define NRF_BOOT_CMD_START	0x02
+#define NRF_BOOT_CMD_WRITE(len)	(0x03 | (len << 4))
+#define NRF_BOOT_CMD_FINALIZE	0x04
+#define NRF_BOOT_CMD_MASK	0x0f
+#define NRF_BOOT_SIZE(v)	(v >> 4)
+struct nrf_boot {
+	uint8_t cmd;
+	uint8_t cksum;
+	uint8_t payload[10];
+};
+
 union nrf_msg {
 	uint8_t generic[12];
 	struct nrf_power power;
+	struct nrf_boot boot;
 };
 
 struct nrf_frame {
